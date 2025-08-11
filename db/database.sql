3185d68d5993bae07a3848fe9576fdc4f0d48630
cronox808 <187470821+cronox808@users.noreply.github.com>
2025-08-11 12:55:16 +0000

    feat: :sparkles: se creo la esctructura del src

--- db/database.sql
+++ db/database.sql
@@ -30,19 +30,35 @@ VALUES (
         SHA2('h3ll0.', 512)
     );
 
-CREATE TABLE plantas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    categoria ENUM('cactus', 'ornamental', 'fruta')NOT NULL,
-    familia VARCHAR(100) NOT NULL,
-    proximo_riego DATE NOT NULL,
-    creaado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+DROP TABLE IF EXISTS `plantas`;
+
+CREATE TABLE IF NOT EXISTS `plantas` (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100) NOT NULL,
+  categoria ENUM('cactus','ornamental','frutal') NOT NULL,
+  familia VARCHAR(100) NOT NULL,
+  proximo_riego DATE NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE riegos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    planta_id INT NOT NULL,
-    fecha_riego DATE NOT NULL,
-    creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (planta_id) REFERENCES plantas(id) ON DELETE CASCADE
+DROP TABLE IF EXISTS `riegos`;
+CREATE TABLE IF NOT EXISTS `riegos` (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  planta_id INT NOT NULL,
+  fecha_riego DATE NOT NULL,
+  creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (planta_id) REFERENCES plantas(id) ON DELETE CASCADE
 );
+
+
+INSERT INTO plantas (nombre, categoria, familia, proximo_riego) VALUES
+('Aloe Vera','cactus','Asphodelaceae','2025-08-14'),
+('Lavanda','ornamental','Lamiaceae','2025-08-07'),
+('Fresa','frutal','Rosaceae','2025-08-09'),
+('Lengua de suegra','ornamental','Asparagaceae','2025-08-07'),
+('Nopal','cactus','Cactaceae','2025-08-14'),
+('Tomatera','frutal','Solanaceae','2025-08-09'),
+('Orquídea','ornamental','Orchidaceae','2025-08-07'),
+('Higuera','frutal','Moraceae','2025-08-09'),
+('Sansevieria','ornamental','Asparagaceae','2025-08-07'),
+('Pitahaya','cactus','Cactaceae','2025-08-14');
